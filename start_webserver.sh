4f7777fbc4471e15af0267e46152013b0c9fc99b
Geo Pertea <geo.pertea@gmail.com>
2024-10-17 10:38:53 -0400

    simplified start_webserver.sh to simply use the docker container

--- start_webserver.sh
+++ start_webserver.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 cd /opt/gpt-researcher
+## just use docker-compose for the new UI, even though that only works locally
+docker-compose up --build
+exit
+## --------------------------
+## --- ignore the method below only works for the simple static UI
 source venv/bin/activate
 ## use --port=6666 to change port to 6666
 uvicorn main:app --host=0.0.0.0 --reload &
-## for the complex server-based web ui:
-cd frontend/nextjs
-npm run dev -- -H 0.0.0.0
+## for the server-based web ui (does not really work):
+#cd frontend/nextjs
+#npm run dev -- -H 0.0.0.0
+##
 
